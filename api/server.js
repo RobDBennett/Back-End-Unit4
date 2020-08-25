97ad64242dc93ee58c2cccaf31acdc485179eec9
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-08-24 23:02:34 -0400

    authentication complete ready to deploy

---
+++ api/server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const session = require('express-session');
+const knexSessionStore = require('connect-session-knex')(session);
+
+const authRouter = require('../auth/authRouter.js');
+const userRouter = require('../users/userRouter.js');
+
+const authenticate = require('../auth/authenticate.js');
+
+const server = express();
+
+
+const sessionConfig = {
+  name: 'auth',
+  secret: 'authenticateUser',
+  cookie: {
+      maxAge: 1000 * 60 * 60,
+      secure: false,
+      httpOnly: true
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new knexSessionStore(
+      {
+          knex: require('../database/db-config.js'),
+          tablename: 'sessions',
+          sidfieldname: 'sid',
+          createtable: true,
+          clearInterval: 1000 * 60 * 60
+      }
+  )
+};
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use(session(sessionConfig));
+
+server.use('/api/auth', authRouter);
+server.use('/api/users', authenticate, userRouter);
+
+server.use('/', (req, res) => {
+    res.send(`
+        <h2>Hey your API is up</h2>
+    `);
+  });
+
+  server.use((err, req, res, next) => {
+    res.status(err.code).json(err)
+  });
+
+//   app.use((err, req, res, next) => {
+//     res.status(err.code).json(err);
+//   });
+
+module.exports = server;
---
+++ database/migrations/20200824205017_data-schema.js
@@ -0,0 +1,16 @@
+const { table } = require('../db-config');
+
+exports.up = function(knex) {
+    return knex.schema
+    .createTable('users', tbl => {
+        tbl.increments();
+        tbl.string('username').unique().notNullable();
+        tbl.string('password').notNullable();
+    })
+
+};
+
+exports.down = function(knex) {
+  return knex.schema
+  .dropTableIfExists('users')
+};
