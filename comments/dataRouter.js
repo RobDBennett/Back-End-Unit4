9e149b0b80664ed2690e8e3f3f5988acf752f86a
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-08-27 17:38:26 -0400

    Update dataRouter.js

--- comments/dataRouter.js
+++ comments/dataRouter.js
@@ -9,40 +9,41 @@ const { response } = require('../api/server.js');
 // Base URL /api/save
 
 
-
 // Get Requests
 // router.get('/', (req, res) => {
 //     const requestOptions = {
 //         headers: {accept: 'application/json'},
 //     };
 
-//     axios
-//         .get('https://unit3-build-dummy-api.herokuapp.com/feed', requestOptions)
-//         .then(async response => {
-//             const feed = response.data.map(comment => {
-//                 return {
-//                     author: comment.author,
-//                     text: comment.text,
-//                     tox: comment.tox
-//                 }
-//             })
+    axios
+        .get('https://jsonblob.com/api/jsonblob/6dd45faf-e7db-11ea-bb30-ff4f3d65208e', requestOptions)
+        .then(async response => {
+            const feed = response.data.map(comment => {
+                return {
+                    author: comment.author,
+                    text: comment.text,
+                    tox: comment.tox
+                }
+            })
 
-//             feed.splice(400, feed.length - 400);
-//             const newFeed = await db.batchInsert("data", feed)
-//                 .then(() => db("data"));
-//             res.status(201).json(newFeed)
+            feed.splice(400, feed.length - 400);
+            const newFeed = await db.batchInsert("data", feed)
+                .then(() => db("data"));
+            res.status(201).json(newFeed)
             
-//         })
-//         .catch(err => {
-//             res.status(500).json({message: 'Error Fetching Data', error: err})
-//         });
-// });
-
-router.get('/', (req, res) => {
-    db.select('*')
-        .from('data')
-        .then(data => res.status(200).json({data: data}))
-        .catch((err) => console.log(err));
+        })
+        .catch(err => {
+            res.status(500).json({message: 'Error Fetching Data', error: err})
+        });
 });
+
+// https://unit3-build-dummy-api.herokuapp.com/feed
+
+// router.get('/', (req, res) => {
+//     db.select('*')
+//         .from('data')
+//         .then(data => res.status(200).json({data: data}))
+//         .catch((err) => console.log(err));
+// });
 
 module.exports = router;
